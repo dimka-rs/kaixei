af1f9345d19844c94cc985e0a1a7937669bfe546
Dmitry K <dimka.rs@gmail.com>
2022-12-02 21:55:59 +0300

    Fix layout, add loops for buttons and pages

--- kaixei.py
+++ kaixei.py
@@ -3,11 +3,22 @@
 import tkinter as tk
 from tkinter import ttk
 
-width=800
-height=480
+# buttons on page
+class Page:
+    buttons = list()
+
+# chapter with pages
+class Chapter:
+    pages = list()
+
+# book with chapters
+class Book:
+    chapters = list()
+
+width=1024
+height=600
 rootwindowx=10
 rootwindowy=10
-
 nbpadw=10
 nbpadh=20
 statush=20
@@ -21,6 +32,7 @@ def btn_cb():
 root = tk.Tk()
 root.geometry(f'{width}x{height}+{rootwindowx}+{rootwindowy}')
 root.title('kaixei')
+root.resizable(width=None, height=None)
 
 style = ttk.Style(root)
 style.configure('TNotebook', tabposition='ws')
@@ -29,12 +41,12 @@ style.configure("TButton", padding=20) # background="#c00"
 style.configure("TFrame", padding=60)
 
 # create a notebook
-notebook = ttk.Notebook(root, style='TNotebook')
-notebook.pack(expand=True) ## add pady=10 ?
+notebook = ttk.Notebook(root, style='TNotebook', width=width-nbpadw, height=height-statush)
+notebook.pack(side='top', fill='both') ## pady=10, expand=True
 
 # create frames
-f1 = ttk.Frame(notebook, width=width-nbpadw, height=height-statush, padding=10)
-f2 = ttk.Frame(notebook, width=width-nbpadw, height=height-statush, padding=10)
+f1 = ttk.Frame(notebook, padding=10)
+f2 = ttk.Frame(notebook, padding=10)
 
 f1.pack(fill='both', expand=True)
 f2.pack(fill='both', expand=True)
@@ -44,25 +56,18 @@ notebook.add(f1, text='Frame1')
 notebook.add(f2, text='Frame2')
 
 # add buttons to frames
-b1a = ttk.Button(f1, text="Button 1 A", command=btn_cb)
-b1a.pack(expand=True)
-b1b = ttk.Button(f1, text="Button 1 B", command=btn_cb)
-b1b.pack(expand=True)
-b1c = ttk.Button(f1, text="Button 1 C", command=btn_cb)
-b1c.pack(expand=True)
+chapter1 = Chapter()
+for pg in range(2):
+    chapter1.pages.append(Page())
 
-# FIXME: adding these shrinks frame to buttons area. Why?
-'''
-b2a = ttk.Button(f2, text="Button 2 A", command=btn_cb)
-b2a.pack(expand=True)
-b2b = ttk.Button(f2, text="Button 2 B", command=btn_cb)
-b2b.pack(expand=True)
-b2c = ttk.Button(f2, text="Button 2 C", command=btn_cb)
-b2c.pack(expand=True)
-'''
+    for btn in range(16):
+        chapter1.pages[pg].buttons.append(ttk.Button(f1 if pg==0 else f2, text=f'Button {str(pg)} {str(btn)}', command=btn_cb))
+        chapter1.pages[pg].buttons[-1].grid(padx=10, pady=10, row=btn//4, column=btn%4)
+
 
 # add status bar
-status = tk.Label(root, text="kaixei status")
-status.pack(fill='x', expand=True, side='bottom')
+status = tk.Label(root, text="kaixei status", bg='#101010')
+#status.pack(fill='x', expand=True, side='bottom')
+status.pack(side='bottom', fill='x')
 
 root.mainloop()
